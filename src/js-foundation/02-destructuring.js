d679f3e23b538bdb105e528a89d0e03edfb5caee
Diego Urrego <diegourrg@gmail.com>
2023-10-31 22:00:56 -0500

    Project exported to typescript.

--- src/js-foundation/02-destructuring.js
+++
@@ -1,8 +0,0 @@
-// console.log(process.env);
-
-const { SHELL, HOMEBREW_PREFIX } = process.env;
-// console.table({ SHELL, HOMEBREW_PREFIX });
-
-const characters = ['Flash', 'Superman', 'Green Lantern', 'Batman'];
-const [, , , batman] = characters;
-// console.log(batman);
